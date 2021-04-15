3ccf37fe0b76004c70d2b0f34bb0826c090f9388
ljc1928 <ljc1928@naver.com>
2021-04-16 00:01:51 +0900

    6070

---
+++ CodeUp_6070.py
@@ -0,0 +1,15 @@
+# 월이 입력될 때 계절 이름이 출력
+
+# 월 : 계절 이름
+# 12, 1, 2 : winter
+#   3, 4, 5 : spring
+#   6, 7, 8 : summer
+#   9, 10, 11 : fall
+
+month = input()
+month = int(month)
+
+if month // 3 == 1: print('spring')
+elif month // 3 == 2: print('summer')
+elif month // 3 == 3: print('fall')
+else: print('winter')
